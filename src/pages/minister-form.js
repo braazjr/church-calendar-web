628f35fa426ffd038aee49a718a3659d06a7c95c
Junior Braz <braz@Juniors-MacBook-Pro.local>
2021-11-19 19:06:37 -0300

    feat: showing color

--- src/pages/minister-form.js
+++ src/pages/minister-form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Card, Form, Button, Badge } from 'react-bootstrap';
+import { Row, Col, Card, Form, Button, Badge, InputGroup, FormControl } from 'react-bootstrap';
 import { Formik } from 'formik';
 
 import Aux from "../hoc/_Aux";
@@ -32,6 +32,10 @@ class MinisterForm extends React.Component {
         minister.functions = minister.functions.map(f => f.name)
         delete minister.newFunction
         console.log('minister', minister)
+    }
+
+    generateNewColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     }
 
     render() {
@@ -77,15 +81,33 @@ class MinisterForm extends React.Component {
                                                             </Form.Group>
                                                         </Col>
                                                         <Col md={6}>
-                                                            <Form.Group controlId="exampleForm.ControlInput1">
-                                                                <Form.Label>color</Form.Label>
-                                                                <Form.Control
-                                                                    type="text"
-                                                                    placeholder="cor"
+                                                            <label style={{
+                                                                marginBottom: '0.3rem'
+                                                            }}>
+                                                                cor
+                                                            </label>
+                                                            <InputGroup
+                                                                style={{
+                                                                    borderRadius: '20px'
+                                                                }}>
+                                                                <InputGroup.Prepend>
+                                                                    <InputGroup.Text onClick={() => {
+                                                                        setFieldValue('color', this.generateNewColor())
+                                                                    }}>
+                                                                        <i className="feather icon-refresh-cw" />
+                                                                    </InputGroup.Text>
+                                                                </InputGroup.Prepend>
+                                                                <FormControl
+                                                                    placeholder="selecione uma cor"
+                                                                    aria-label="cor"
                                                                     name="color"
                                                                     onChange={handleChange}
-                                                                    value={values.color} />
-                                                            </Form.Group>
+                                                                    value={values.color}
+                                                                    style={{
+                                                                        backgroundColor: values.color
+                                                                    }}
+                                                                />
+                                                            </InputGroup>
                                                         </Col>
                                                         <Col md={12}>
                                                             <Form.Group controlId="exampleForm.ControlInput1">
